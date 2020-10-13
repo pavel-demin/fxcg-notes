727ba706c92605e9a06ac7ea8d195f1fc5b0e5ee
Pavel Demin <pavel-demin@outlook.com>
2020-10-13 09:49:50 +0200

    update mkg3a repository

--- scripts/install-mkg3a.sh
+++ scripts/install-mkg3a.sh
@@ -2,7 +2,7 @@ prefix=/opt/casio
 
 mkdir -p mkg3a mkg3a-build
 
-curl https://bitbucket.org/tari/mkg3a/get/default.tar.gz | tar -zx --strip-components=1 --directory=mkg3a
+curl https://gitlab.com/taricorp/mkg3a/-/archive/master/mkg3a-master.tar.gz | tar -zx --strip-components=1 --directory=mkg3a
 
 cd mkg3a-build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix ../mkg3a
