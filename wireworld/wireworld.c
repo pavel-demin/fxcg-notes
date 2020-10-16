aff34ac273a6df87c8d739bd1a55709c68b280df
Pavel Demin <pavel-demin@outlook.com>
2020-10-16 18:31:34 +0200

    enable edit keys in wireworld

--- wireworld/wireworld.c
+++ wireworld/wireworld.c
@@ -10,7 +10,7 @@
 #define COLS 64
 #define ROWS 33
 
-int flag;
+int generation, running, wire, electron, x, y;
 
 color_t *vram;
 
@@ -46,6 +46,16 @@ void box(int x, int y, int w, int h, int color)
   }
 }
 
+void cross(int x, int y, int color)
+{
+  int i;
+  for(i = 0; i < 5; ++i)
+  {
+    vram[(y + 2) * LCD_WIDTH_PX + x + i] = color;
+    vram[(y + i) * LCD_WIDTH_PX + x + 2] = color;
+  }
+}
+
 void transition(const char *in, char *out, int w, int h)
 {
   int i, j, k, l;
@@ -56,44 +66,105 @@ void transition(const char *in, char *out, int w, int h)
     for(j = 0; j < h; ++j)
     {
       index = j * w + i;
-      switch(in[index])
+      if(i == x && j == y && wire)
       {
-      case 't':
-        out[index] = '.';
-        color = COLOR_BLUE;
-        break;
-      case 'H':
-        out[index] = 't';
-        color = COLOR_YELLOW;
-        break;
-      case '.':
-        counter = 0;
-        for(k = i - 1; k < i + 2; ++k)
+        wire = 0;
+        switch(in[index])
         {
-          if(k < 0 || k >= w) continue;
-          for(l = j - 1; l < j + 2; ++l)
-          {
-            if(l < 0 || l >= h) continue;
-            counter += (in[l * w + k] == 'H');
-          }
-        }
-        if(counter == 1 || counter == 2)
-        {
-          out[index] = 'H';
-          color = COLOR_RED;
-        }
-        else
-        {
+        case 't':
+          out[index] = ' ';
+          color = COLOR_BLACK;
+          break;
+        case 'H':
+          out[index] = ' ';
+          color = COLOR_BLACK;
+          break;
+        case '.':
+          out[index] = ' ';
+          color = COLOR_BLACK;
+          break;
+        default:
           out[index] = '.';
           color = COLOR_BLUE;
+          break;
         }
-        break;
-      default:
+      }
+      else if(i == x && j == y && electron)
+      {
+        electron = 0;
+        switch(in[index])
+        {
+        case 'H':
+          out[index] = '.';
+          color = COLOR_BLUE;
+          break;
+        default:
+          out[index] = 'H';
+          color = COLOR_RED;
+          break;
+        }
+      }
+      else if(running)
+      {
+        switch(in[index])
+        {
+        case 't':
+          out[index] = '.';
+          color = COLOR_BLUE;
+          break;
+        case 'H':
+          out[index] = 't';
+          color = COLOR_YELLOW;
+          break;
+        case '.':
+          counter = 0;
+          for(k = i - 1; k < i + 2; ++k)
+          {
+            if(k < 0 || k >= w) continue;
+            for(l = j - 1; l < j + 2; ++l)
+            {
+              if(l < 0 || l >= h) continue;
+              counter += (in[l * w + k] == 'H');
+            }
+          }
+          if(counter == 1 || counter == 2)
+          {
+            out[index] = 'H';
+            color = COLOR_RED;
+          }
+          else
+          {
+            out[index] = '.';
+            color = COLOR_BLUE;
+          }
+          break;
+        default:
+          out[index] = in[index];
+          color = COLOR_BLACK;
+          break;
+        }
+      }
+      else
+      {
         out[index] = in[index];
-        color = COLOR_BLACK;
-        break;
+        switch(in[index])
+        {
+        case 't':
+          color = COLOR_YELLOW;
+          break;
+        case 'H':
+          color = COLOR_RED;
+          break;
+        case '.':
+          color = COLOR_BLUE;
+          break;
+        default:
+          color = COLOR_BLACK;
+          break;
+        }
       }
       if(color != COLOR_BLACK) box(1 + i * 6, 1 + j * 6, 5, 5, color);
+      if(i == x && j == y) cross(1 + i * 6, 1 + j * 6, COLOR_WHITE);
     }
   }
 }
@@ -127,7 +198,14 @@ void copy(const char *in, int size, char *out, int w, int h)
 
 void menu()
 {
-  print(0, LCD_HEIGHT_PX - 16, COLOR_BLACK, COLOR_WHITE, "Start");
+  if(running)
+  {
+    print(0, LCD_HEIGHT_PX - 16, COLOR_BLACK, COLOR_WHITE, "Stop ");
+  }
+  else
+  {
+    print(0, LCD_HEIGHT_PX - 16, COLOR_BLACK, COLOR_WHITE, "Start");
+  }
   print(64, LCD_HEIGHT_PX - 16, COLOR_BLACK, COLOR_WHITE, " Save");
   print(128, LCD_HEIGHT_PX - 16, COLOR_BLACK, COLOR_WHITE, " Load");
   print(192, LCD_HEIGHT_PX - 16, COLOR_BLACK, COLOR_WHITE, " Wire");
@@ -137,10 +215,10 @@ void menu()
 void timeout()
 {
   memset(vram, 0, LCD_WIDTH_PX * (LCD_HEIGHT_PX - 16) * 2);
-  transition(world[flag], world[1 - flag], COLS, ROWS);
+  transition(world[generation], world[1 - generation], COLS, ROWS);
   menu();
   Bdisp_PutDisp_DD();
-  flag = 1 - flag;
+  generation = 1 - generation;
 }
 
 void load(int fd)
@@ -152,7 +230,7 @@ void load(int fd)
   size = Bfile_ReadFile_OS(fd, buffer, size, 0);
   Bfile_CloseFile_OS(fd);
 
-  flag = 0;
+  generation = 0;
 
   memset(world[0], ' ', COLS * ROWS);
   memset(world[1], ' ', COLS * ROWS);
@@ -165,7 +243,12 @@ int main()
   int fd;
   int key, timer;
 
-  flag = 0;
+  generation = 0;
+  running = 1;
+  wire = 0;
+  electron = 0;
+  x = COLS - 1;
+  y = ROWS - 1;
 
   memset(world[0], ' ', COLS * ROWS);
   memset(world[1], ' ', COLS * ROWS);
@@ -183,6 +266,9 @@ int main()
     GetKey(&key);
     switch(key)
     {
+    case KEY_CTRL_F1:
+      running = 1 - running;
+      break;
     case KEY_CTRL_F3:
       Timer_Stop(timer);
       Timer_Deinstall(timer);
@@ -192,11 +278,23 @@ int main()
       timer = Timer_Install(0, timeout, 100);
       Timer_Start(timer);
       break;
-    case KEY_CTRL_F5:
-      print(0, LCD_HEIGHT_PX - 16, COLOR_BLACK, COLOR_WHITE, "Stop ");
+    case KEY_CTRL_F4:
+      wire = 1;
       break;
-    case KEY_CTRL_F6:
-      print(0, LCD_HEIGHT_PX - 16, COLOR_BLACK, COLOR_WHITE, "Start");
+    case KEY_CTRL_F5:
+      electron = 1;
+      break;
+    case KEY_CTRL_UP:
+      if(y > 0) --y;
+      break;
+    case KEY_CTRL_DOWN:
+      if(y < ROWS - 1) ++y;
+      break;
+    case KEY_CTRL_LEFT:
+      if(x > 0) --x;
+      break;
+    case KEY_CTRL_RIGHT:
+      if(x < COLS - 1) ++x;
       break;
     }
   }
