b638eeb2fecc8159e42f4b267cb37e84f065e510
Pavel Demin <pavel-demin@outlook.com>
2021-11-15 00:23:16 +0100

    fix next in rushhour.c

--- rushhour/rushhour.c
+++ rushhour/rushhour.c
@@ -408,7 +408,7 @@ int main()
       }
       break;
     case KEY_CTRL_F6:
-      if(data.curr + 72 < data.size)
+      if(data.curr + 71 < data.size)
       {
         data.curr += 36;
         decode(data.buffer + data.curr);
